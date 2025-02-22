b93c8782b9338018735cfb42ead6c44af1f5d6ab
Gurpreet Singh <gurpreet4793@gmail.com>
2025-02-22 14:29:01 +0530

    Add TS file.

---
+++ TypeScript/hello_world.js
@@ -0,0 +1,6 @@
+// filepath: /home/gursing/practise/practice/TypeScript/hello_world.ts
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+var greeting = greet("World");
+console.log(greeting);
---
+++ TypeScript/hello_world.ts
@@ -0,0 +1,7 @@
+// filepath: /home/gursing/practise/practice/TypeScript/hello_world.ts
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+const greeting = greet("World");
+console.log(greeting);
