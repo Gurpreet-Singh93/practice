b3138aec07997ddac384abce5ee815217a10b677
Gurpreet Singh <gurpreet4793@gmail.com>
2025-02-22 14:32:28 +0530

    Add git ignore file.

--- TypeScript/hello_world.js
+++
@@ -1,6 +0,0 @@
-// filepath: /home/gursing/practise/practice/TypeScript/hello_world.ts
-function greet(name) {
-    return "Hello, ".concat(name, "!");
-}
-var greeting = greet("World");
-console.log(greeting);
